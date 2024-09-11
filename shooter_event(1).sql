942b8854fd4d3a353def9c08c7a675e4b32a3363
Khairol Anwar <whyuwanttoknowm@gmail.com>
2024-09-11 10:34:03 +0800

    updated database

--- shooter_event(1).sql
+++ shooter_event(1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 10, 2024 at 09:52 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Sep 11, 2024 at 04:33 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,6 +32,14 @@ CREATE TABLE `centrefire` (
   `Participant_ID` int(10) NOT NULL,
   `TotalScore` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `centrefire`
+--
+
+INSERT INTO `centrefire` (`CentreFire_ID`, `Participant_ID`, `TotalScore`) VALUES
+(11, 21, 28),
+(12, 22, 28);
 
 -- --------------------------------------------------------
 
@@ -61,7 +69,9 @@ CREATE TABLE `participant` (
 --
 
 INSERT INTO `participant` (`Participant_ID`, `name`, `Group_ID`) VALUES
-(21, 'aiman', 21);
+(21, 'aiman', 21),
+(22, 'Test', 21),
+(23, 'tester', 21);
 
 -- --------------------------------------------------------
 
@@ -87,6 +97,33 @@ CREATE TABLE `score` (
   `ScoreName` varchar(100) NOT NULL,
   `Participant_ID` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `score`
+--
+
+INSERT INTO `score` (`Score_ID`, `Score`, `ScoreName`, `Participant_ID`) VALUES
+(204, 1, 'Score1', 21),
+(205, 2, 'Score2', 21),
+(206, 3, 'Score3', 21),
+(207, 4, 'Score4', 21),
+(208, 5, 'Score5', 21),
+(209, 6, 'Score6', 21),
+(210, 8, 'Score7', 21),
+(218, 2, 'Score1', 22),
+(219, 2, 'Score2', 22),
+(220, 3, 'Score3', 22),
+(221, 4, 'Score4', 22),
+(222, 5, 'Score5', 22),
+(223, 6, 'Score6', 22),
+(224, 7, 'Score7', 22),
+(232, 1, 'Score1', 23),
+(233, 0, 'Score2', 23),
+(234, 0, 'Score3', 23),
+(235, 0, 'Score4', 23),
+(236, 0, 'Score5', 23),
+(237, 0, 'Score6', 23),
+(238, 0, 'Score7', 23);
 
 --
 -- Indexes for dumped tables
@@ -124,6 +161,7 @@ ALTER TABLE `platescore`
 --
 ALTER TABLE `score`
   ADD PRIMARY KEY (`Score_ID`),
+  ADD UNIQUE KEY `Participant_ID` (`Participant_ID`,`ScoreName`),
   ADD KEY `Participant_ID_fk` (`Participant_ID`);
 
 --
@@ -134,7 +172,7 @@ ALTER TABLE `score`
 -- AUTO_INCREMENT for table `centrefire`
 --
 ALTER TABLE `centrefire`
-  MODIFY `CentreFire_ID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `CentreFire_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `group`
@@ -146,19 +184,19 @@ ALTER TABLE `group`
 -- AUTO_INCREMENT for table `participant`
 --
 ALTER TABLE `participant`
-  MODIFY `Participant_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `Participant_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `platescore`
 --
 ALTER TABLE `platescore`
-  MODIFY `PlateScore_ID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `PlateScore_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `score`
 --
 ALTER TABLE `score`
-  MODIFY `Score_ID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `Score_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=239;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
